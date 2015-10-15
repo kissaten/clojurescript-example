8b74bccc63c6af0f595d5ab18c7b9e08692da297
Joe Kutner <jpkutner@gmail.com>
2015-10-15 14:38:05 -0500

    Contacts

--- src/cljs/my_app/core.cljs
+++ src/cljs/my_app/core.cljs
@@ -4,14 +4,32 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state
+  (atom
+   {:contacts
+    [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+     {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+(defn display-name [{:keys [first last] :as contact}]
+    (str last ", " first))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h2 nil "Contact list")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts data)))))))
 
 (defn main []
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (:text app)))))
+    contacts-view
     app-state
     {:target (. js/document (getElementById "app"))}))
